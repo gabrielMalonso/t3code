541fb319b72729041b039ed5f24fb140fb862729
gabrielMalonso <gabriel_alonso_@outlook.com>
2026-04-15 20:27:15 -0300

    Extract chat file reference and skill menu slots
    - Move user file reference chips into `t3code-custom/chat`
    - Share composer skill menu item building from custom hooks

--- apps/web/src/t3code-custom/hooks/index.ts
+++ apps/web/src/t3code-custom/hooks/index.ts
@@ -1,5 +1,8 @@
 export { useThreadLoopActions } from "./useThreadLoopActions";
 export { useComposerPasteFileReference } from "./useComposerPasteFileReference";
 export { useComposerFileReferenceSend } from "./useComposerFileReferenceSend";
-export { useComposerProviderSkills } from "./useComposerProviderSkills";
+export {
+  buildComposerSkillMenuItems,
+  useComposerProviderSkills,
+} from "./useComposerProviderSkills";
 export { useComposerSendExtension } from "./useComposerSendExtension";
